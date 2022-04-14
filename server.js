2862b65881df6b12f7a56f71630eddd29107d017
Abbas <abbaskalantari73@gmail.com>
2022-04-14 09:22:54 -0400

    adding the first model- book

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
-const indexRouter = require('./routes/index');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
 //connecting mongodb
@@ -28,6 +29,12 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error) )
 db.once('open', ()=> console.log('Connected to Mongoose'))
 
+// Importing Routers
+const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+
+// Setting Routes
+app.use('/authors', authorsRouter)
 app.use('/', indexRouter)
 
 
